05ce6bb9bc0ff63867370a5c94ebd5440c7c3aac
Marcus-U <marcusuhthoff@gmail.com>
2024-11-15 14:10:21 -0500

    testing

---
+++ data_collection.py
@@ -0,0 +1,50 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import pybaseball
+from pybaseball import statcast
+pybaseball.cache.enable()
+
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+def collect_pitch_data(start, end, filename = "", save_file = False):
+    '''
+    
+    '''
+    #TODO: check date time formats
+    df = statcast(start_dt= start ,end_dt= end).iloc[::-1].reset_index(drop=True)
+
+    if save_file:
+        df.to_csv(filename, index = False)
+
+    return df
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
